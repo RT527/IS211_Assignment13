52cca370dff6aa8aa844e81d4902cd4006bafd81
“RT527” <rafi.talukder7@gmail.com>
2025-11-30 18:49:56 -0500

    Finish

--- recursion.py
+++ recursion.py
@@ -11,3 +11,18 @@ def gcd(a, b): #This computes the greatest common divisor (GCD) of two integers 
     if b == 0:
         return a
     return gcd(b, a % b)
+"""---------------------------------------------------------------------------------"""
+def compareTo(s1, s2): #This compares two strings recursively
+
+    if s1 == "" and s2 == "":
+        return 0 #If both strings are empty, they are equal
+
+    if s1 == "" and s2 != "":
+        return -1
+    if s1 != "" and s2 == "":
+        return 1 #If one string is empty, the shorter one is considered smaller
+
+    if s1[0] != s2[0]:
+        return ord(s1[0]) - ord(s2[0]) #This compares the first characters of each string
+
+    return compareTo(s1[1:], s2[1:])#This compare the rest of the strings
