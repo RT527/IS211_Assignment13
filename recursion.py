58f1f0ff904e42f116ad2a137af9818559f1b2a2
“RT527” <rafi.talukder7@gmail.com>
2025-11-30 18:39:34 -0500

    add gcd

--- recursion.py
+++ recursion.py
@@ -6,3 +6,8 @@ def fibonnaci(n): #This returns the nth Fibonacci number using recursion.The seq
     if n == 1 or n == 2:
         return 1
     return fibonnaci(n - 1) + fibonnaci(n - 2)
+"""---------------------------------------------------------------------------------"""
+def gcd(a, b): #This computes the greatest common divisor (GCD) of two integers using Euclid's recursive algorithm.
+    if b == 0:
+        return a
+    return gcd(b, a % b)
