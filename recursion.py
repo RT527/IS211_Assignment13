4da4f0fc133bc094147e37f6fb29a22dcce41dbe
“RT527” <rafi.talukder7@gmail.com>
2025-11-30 18:25:06 -0500

    Add fibonnaci

--- recursion.py
+++ recursion.py
@@ -1,3 +1,8 @@
 # Rafi Talukder Assignment_13
 
 """---------------------------------------------------------------------------------"""
+
+def fibonnaci(n): #This returns the nth Fibonacci number using recursion.The sequence starts with 1, 1 for n = 1 and n = 2.
+    if n == 1 or n == 2:
+        return 1
+    return fibonnaci(n - 1) + fibonnaci(n - 2)
